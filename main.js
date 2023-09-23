bbd88efd2ca911b7a18293f4daa9acebe81752a7
Esdras Nehemias <esdrasmormon20@gmail.com>
2023-09-23 08:05:39 -0600

    Creamos el motor de plantillas

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const main = express();
 
+main.set('view engine', 'ejs');
+
 main.listen(5000, ()=> {
     console.log('SERVIDOR EJECUTANDOSE: http://localhost:5000');
 });
