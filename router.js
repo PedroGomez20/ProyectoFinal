5a89d80a28ec04a8555dbc2a022e7bf8f610b976
EsdrasNehemias2001 <esdrasmormon20@gmail.com>
2023-09-23 16:41:49 -0600

    Se enruta el archivo index y se agrega plantilla desde bootstrap

--- router.js
+++ router.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const conexion = require('./database/db');
 
 router.get('/', (req, res) => {
-    conexion.query('SELECT * FROM productos', (error, resultados) => {
+    res.render('index');
+/*     conexion.query('SELECT * FROM productos', (error, resultados) => {
         if(error) {
             throw error;
         } else {
             res.send(resultados);
         }
-    })
+    }) */
 })
 
 module.exports = router;
