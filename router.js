38e9ef91090f3bc584e26be6ca722fc377bf80a3
EsdrasNehemias2001 <esdrasmormon20@gmail.com>
2023-09-25 10:18:34 -0600

    Se enruta el metodo crear en archivo router.js y se realiza una prueba

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const conexion = require('./database/db');
 
+//MOSTRAR TODOS LOS REGISTROS
 router.get('/', (req, res) => {
     conexion.query('SELECT * FROM productos', (error, resultados) => {
         if (error) {
@@ -11,6 +12,11 @@ router.get('/', (req, res) => {
             res.render('index', { resultados: resultados });
         }
     })
+})
+
+//RUTA PARA CREAR REGISTROS
+router.get('/create', (req, res) => {
+    res.render('create');
 })
 
 module.exports = router;
